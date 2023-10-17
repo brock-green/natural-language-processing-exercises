862d5d9208e2a4b08a15e8a5177c28ede3124e8d
Brock Green <brock.green03@gmail.com>
2023-10-17 16:20:31 -0500

    update

--- acquire.py
+++ acquire.py
@@ -1,95 +1,126 @@
 # imports
 import numpy as np
+import pandas as pd
 import os
 import requests
 from bs4 import BeautifulSoup
 import re
 import random
+import time
+
+#####################################################################################################################
 
 def get_blog_articles():
-    '''
-    
-    '''
-    # define some user_agents to spoof the request headers
+    """
+    Scrapes and structures blog data from the Codeup website.
+
+    This function sends requests to the Codeup blog website, retrieves blog links,
+    and extracts article titles and content. It then organizes the data into a
+    Pandas DataFrame.
+
+    Returns:
+    pandas.DataFrame: A DataFrame containing the blog data with columns 'title' and 'article'.
+    """
+    # Define user agents for request headers
     user_agents = [
-      "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0",
-      "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0",
-      "Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0"
-      ]
-
-
-    # randomize the user agent
-    random_user_agent = random.choice(user_agents)
-    
-    # record the urls for at least 5 distinct blog posts.
-    url0 = 'https://codeup.edu/featured/apida-heritage-month/'
-    url1 = 'https://codeup.edu/featured/women-in-tech-panelist-spotlight/'
-    url2 = 'https://codeup.edu/featured/women-in-tech-rachel-robbins-mayhill/'
-    url3 = 'https://codeup.edu/codeup-news/women-in-tech-panelist-spotlight-sarah-mellor/   '
-    url4 = 'https://codeup.edu/events/women-in-tech-madeleine/'
-    
-    # create response object
-    headers = {'User-Agent': random_user_agent}
-    response0 = requests.get(url0, headers=headers)
-    response1 = requests.get(url1, headers=headers)
-    response2 = requests.get(url2, headers=headers)
-    response3 = requests.get(url3, headers=headers)
-    response4 = requests.get(url4, headers=headers)
-    
-    # create soup object
-    soup0 = BeautifulSoup(response0.text, 'html.parser')
-    soup1 = BeautifulSoup(response1.text, 'html.parser')
-    soup2 = BeautifulSoup(response2.text, 'html.parser')
-    soup3 = BeautifulSoup(response3.text, 'html.parser')
-    soup4 = BeautifulSoup(response4.text, 'html.parser')
-    
-    # use soup.select to find the title
-    title0 = (soup0.h1.text)
-    title1 = (soup1.h1.text)
-    title2 = (soup2.h1.text)
-    title3 = (soup3.h1.text)
-    title4 = (soup4.h1.text)
-    
-    # create vars for lists of all p elements
-    list_of_p0 = soup0.find_all('p')
-    list_of_p1 = soup1.find_all('p')
-    list_of_p2 = soup2.find_all('p')
-    list_of_p3 = soup3.find_all('p')
-    list_of_p4 = soup4.find_all('p')
-    
-    # create vars to hold the contents of the blog posts. Use list comprehension to join the list of elements into a string.
-    contents_blog0 = ' '.join([element.text for element in list_of_p0])
-    contents_blog1 = ' '.join([element.text for element in list_of_p1])
-    contents_blog2 = ' '.join([element.text for element in list_of_p2])
-    contents_blog3 = ' '.join([element.text for element in list_of_p3])
-    contents_blog4 = ' '.join([element.text for element in list_of_p4])
-    
-    # Create a dictionary of title and content for each blog post
-    blog0 = {'title': title0, 'content': contents_blog0}
-    blog1 = {'title': title1, 'content': contents_blog1}
-    blog2 = {'title': title2, 'content': contents_blog2}
-    blog3 = {'title': title3, 'content': contents_blog3}
-    blog4 = {'title': title4, 'content': contents_blog4}
-    
-    # create a list of dictionaries for each blog post
-    blogs = [blog0, blog1, blog2, blog3, blog4]
-    
-    return blogs
-
-
-def get_news_articles():
-  '''
-  
-  '''
-  # define some user_agents to spoof the request headers
-  user_agents = [
-    "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0",
-    "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0",
-    "Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0"
+        "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0",
+        "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0",
+        "Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0"
     ]
 
+    # Randomly select a user agent
+    headers = {'User-Agent': random.choice(user_agents)}
 
-  # randomize the user agent
-  random_user_agent = random.choice(user_agents)
+    # Base codeup blog URL
+    base_url = 'https://codeup.edu/blog/'
+
+    # Get the blog links from the base URL
+    response = requests.get(base_url, headers=headers)
+    base_soup = BeautifulSoup(response.text, 'html.parser')
+    blog_links = [element['href'] for element in base_soup.find_all('a', class_='more-link')]
+
+    all_blogs = []
+
+    for link in blog_links:
+        # Get a response from the blog link
+        response = requests.get(link, headers=headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Extract title and body
+        title = soup.find('h1', class_='entry-title').text
+        body = soup.find('div', class_='entry-content').text.strip()
+
+        # Create a dictionary for each blog post
+        row = {'title': title, 'article': body}
+        all_blogs.append(row)
+
+    # Cast the list of dictionaries into a pandas DataFrame
+    articles = pd.DataFrame(all_blogs)
+
+    return articles
+
   
+  #####################################################################################################################
   
+def get_news_articles():
+    """
+    Scrape articles from predefined categories on a website and structure the data into a Pandas DataFrame.
+
+    Returns:
+        pandas.DataFrame: A DataFrame containing the scraped articles with columns 'title', 'body', and 'category'.
+    """
+    # Define user agents for request headers
+    user_agents = [
+        "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0",
+        "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0",
+        "Mozilla/5.0 (X11; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0"
+    ]
+
+    # Randomly select a user agent
+    headers = {'User-Agent': random.choice(user_agents)}
+
+    # Define the base URL and categories to scrape
+    base_url = 'https://inshorts.com/en/read/'
+    categories = ['business', 'entertainment', 'technology', 'sports']
+
+    all_articles = pd.DataFrame(columns=['title', 'body', 'category'])
+    t_sum = 0
+
+    for category in categories:
+        t_0 = time.time()
+        print(f'Grabbing contents for {category}.')
+        
+        # Construct a URL based on the base concatenated with the category
+        category_url = base_url + category
+        
+        # Get the response text from the constructed URL
+        raw_content = requests.get(category_url, headers=headers).text
+        
+        # Turn the content into soup
+        soup = BeautifulSoup(raw_content, 'html.parser')
+        
+        # Title content
+        titles = [element.text for element in soup.find_all('span', itemprop='headline')]
+        
+        # Body content
+        bodies = [element.text for element in soup.find_all('div', itemprop='articleBody')]
+        
+        # Create a DataFrame for the current category
+        category_df = pd.DataFrame({
+            'title': titles,
+            'body': bodies,
+            'category': category
+        })
+        
+        # Concatenate the current category DataFrame with the overall DataFrame
+        all_articles = pd.concat([all_articles, category_df], axis=0, ignore_index=True)
+        
+        t_n = time.time()
+        t_delta = t_n - t_0
+        print(f'Time to grab contents of {category}: {round(t_delta, 2)} seconds')
+        t_sum += t_delta
+
+    print('Job finished!')
+    print(f'It took {round((t_sum / 60), 2)} minutes to execute scraping')
+
+    return all_articles
